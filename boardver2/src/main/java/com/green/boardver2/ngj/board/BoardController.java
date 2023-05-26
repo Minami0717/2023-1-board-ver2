93c5471f102b1552b5304d33ff40082300ba33a3
남규진 <dft2007@naver.com>
2023-05-26 16:59:48 +0900

    ngj yaml

--- boardver2/src/main/java/com/green/boardver2/ngj/board/BoardController.java
+++ boardver2/src/main/java/com/green/boardver2/ngj/board/BoardController.java
@@ -3,8 +3,8 @@ package com.green.boardver2.ngj.board;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/board")
+@RestController("ngjBoardController")
+@RequestMapping("/ngj/board")
 public class BoardController {
 
 }
--- boardver2/src/main/java/com/green/boardver2/ngj/user/UserController.java
+++ boardver2/src/main/java/com/green/boardver2/ngj/user/UserController.java
@@ -1,4 +1,14 @@
 package com.green.boardver2.ngj.user;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("ngjUserController")
+@RequestMapping("/ngj/user")
 public class UserController {
+    @PostMapping
+    public int postUser() {
+        return 0;
+    }
 }
