8215dc3c49e3cbfeee1c6365a8aeda979d6298f2
정우진 <wash98@naver.com>
2023-05-26 15:55:59 +0900

    jwj 패키지 생성

--- boardver2/src/main/java/com/green/boardver2/boardJWJ/Test.java
+++
@@ -1,5 +0,0 @@
-package com.green.boardver2.boardJWJ;
-
-public class Test {
-    //dd
-}
--- boardver2/src/main/java/com/green/boardver2/boardNGJ/BoardController.java
+++
@@ -1,8 +0,0 @@
-package com.green.boardver2.boardNGJ;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class BoardController {
-    //test123
-}
